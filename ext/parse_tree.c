ac9f00e61d68df6cb08e86c9eb0d3a06569873e5
rocky <rockyb@rubyforge.org>
2010-06-07 02:48:01 -0400

    We now handle NODE_TRUE, NODE_FALSE and NODE_NIL - woohoo!

--- ext/parse_tree.c
+++ ext/parse_tree.c
@@ -3,19 +3,51 @@
 #include "../include/vm_core_mini.h"   /* Pulls in ruby.h and node.h */
 #include "../include/ruby19_externs.h" 
 
-#define D_NULL_NODE wrap_into_node("nil", Qnil);
+/* The below is a merger of dump_node() of Ruby 1.9's node.c and
+   add_to_parse_tree() of ParseTree's parsetree.rb
+
+   As such the macros below may seem weird because they are from node.c
+   but try to do corresponding ParseTree actions.
+*/
+
+#define ANN(ann)    /* Discard annotation */
+#define LAST_NODE   /* Ignore adjustment of node-placement indentation. */
+
+#define D_NULL_NODE wrap_into_node("NODE_NIL", Qnil)
 
 #define D_NODE_HEADER(node)						\
     current = rb_ary_new();						\
-    node_name = ID2SYM(rb_intern(ruby_node_name(nd_type(node))));	\
+    node_name = pt_node_name(ruby_node_name(nd_type(node)));		\
     rb_ary_push(ary, current);						\
     rb_ary_push(current, node_name);
+
+#define F_NODE(name, local) \
+    add_to_parse_tree(self, current, node->name, local)
+
+
+/* Turn a Ruby node string, e.g. "NODE_FALSE" into a symbol that ParseTree
+   uses, e.g. :false. */
+static VALUE 
+pt_node_name(const char *name)
+{
+    if (0 == strncmp(name, "NODE_", strlen("NODE_"))) { 
+	const char * name_sans_node = &name[strlen("NODE_")];
+	return 
+	    rb_funcall(
+		rb_funcall(rb_str_new2(name_sans_node), rb_intern("downcase"), 
+			   0),
+		rb_intern("to_sym"), 0);
+    } else {
+	rb_bug("pt_node_name: node %s name should start with NODE_", name);
+	return Qnil;
+    }
+}
 
 static VALUE 
 wrap_into_node(const char * name, VALUE val) 
 {
     VALUE n = rb_ary_new();
-    rb_ary_push(n, ID2SYM(rb_intern(name)));
+    rb_ary_push(n, pt_node_name(name));
     if (val) rb_ary_push(n, val);
     return n;
 }
@@ -32,13 +64,35 @@ add_to_parse_tree(VALUE self, VALUE ary, NODE *node, ID *locals)
     D_NODE_HEADER(node);
 
     switch (nd_type(node)) {
+
+      case NODE_NIL:
+	ANN("nil");
+	ANN("format: nil");
+	ANN("example: nil");
+	break;
+
+      case NODE_TRUE:
+	ANN("true");
+	ANN("format: true");
+	ANN("example: true");
+	break;
+
+      case NODE_FALSE:
+	ANN("false");
+	ANN("format: false");
+	ANN("example: false");
+	break;
+
       case NODE_SCOPE:
-	add_to_parse_tree(self, current, node->nd_next, node->nd_tbl);
+	ANN("new scope");
+	ANN("format: [nd_tbl]: local table, [nd_args]: arguments, [nd_body]: body");
+	F_NODE(nd_args, node->nd_tbl);
+	LAST_NODE;
+	F_NODE(nd_body, node->nd_tbl);
 	break;
       default:
 	rb_bug("dump_node: unknown node: %s", ruby_node_name(nd_type(node)));
     }
-    /* FIXME */
     return current;
 }
 
